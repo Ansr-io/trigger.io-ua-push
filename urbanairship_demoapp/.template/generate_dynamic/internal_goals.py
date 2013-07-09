a047556cab587ac350d96ca51fdc77cc02b652f2
zack <zackdykes@gmail.com>
2013-07-09 15:56:30 +0100

    update .gitignore, delete ignored

--- urbanairship_demoapp/.template/generate_dynamic/internal_goals.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-LOG = logging.getLogger(__name__)
-
-def config_changes_invalidate_templates(generate, old_config_filename, new_config_filename):
-	return generate.internal_tasks.config_changes_invalidate_templates(
-			generate,
-			old_config_filename,
-			new_config_filename)
