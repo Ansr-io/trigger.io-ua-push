0471f52e45106f9175d518651b8203effc2dcf11
James Brady <james.colin.brady@gmail.com>
2013-08-09 11:41:30 +0100

    remove UAShim - use onApplicationCreate event instead

--- module/inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/urbanairship/EventListener.java
+++ module/inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/urbanairship/EventListener.java
@@ -1,14 +1,23 @@
 package io.trigger.forge.android.modules.urbanairship;
 
-import android.content.Intent;
-import android.os.Bundle;
 import io.trigger.forge.android.core.ForgeApp;
 import io.trigger.forge.android.core.ForgeEventListener;
 
+import com.urbanairship.UAirship;
+import com.urbanairship.location.UALocationManager;
+import com.urbanairship.push.PushManager;
+
 public class EventListener extends ForgeEventListener {
-
+	@Override
+	public void onApplicationCreate() {
+		UAirship.takeOff(ForgeApp.getApp());
+		PushManager.enablePush();
+		UALocationManager.init();
+		PushManager.shared().setIntentReceiver(IntentReceiver.class);
+	}
 	
-
-	
-	
+	@Override
+    public void onStop() {
+        UAirship.land();
+    }
 }
