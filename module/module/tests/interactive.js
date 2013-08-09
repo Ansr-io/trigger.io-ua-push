6684c047b856f74b9bbde151ee20ec24bfefced1
James Brady <james.colin.brady@gmail.com>
2013-08-09 15:41:39 +0100

    remove unnecessary getPushID call from interactive test

--- module/module/tests/interactive.js
+++ module/module/tests/interactive.js
@@ -1,14 +1,5 @@
 module("urbanairship");
 
-asyncTest("Attempt to activate push notifications", 1, function() {
-	forge.urbanairship.getPushID(function (pushID) {
-		ok(pushID, "Push ID received");
-		start();
-	}, function (content) {
-		ok(false, JSON.stringify(content));
-	});
-});
-
 asyncTest("Attempt to receive a push notitication", 1, function() {
 	askQuestion("Create a push notification that this device should receive in the Urban Airship console", {
 		"OK: nothing happened": function () {
