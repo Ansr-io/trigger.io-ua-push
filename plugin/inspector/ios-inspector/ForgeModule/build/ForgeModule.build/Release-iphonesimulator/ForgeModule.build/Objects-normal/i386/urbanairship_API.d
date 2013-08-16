b5c5e3885cae458c151c212a412afb6146910a98
zack <zackdykes@gmail.com>
2013-08-16 11:32:34 +0100

    FAT-37: bump version, update inspectors, build

--- plugin/inspector/ios-inspector/ForgeModule/build/ForgeModule.build/Release-iphonesimulator/ForgeModule.build/Objects-normal/i386/urbanairship_API.d
+++ plugin/inspector/ios-inspector/ForgeModule/build/ForgeModule.build/Release-iphonesimulator/ForgeModule.build/Objects-normal/i386/urbanairship_API.d
@@ -1,14 +1,14 @@
 dependencies: \
-  /Users/pete/Dropbox/forge-workspace/trigger.io-ua-push/trigger.io-ua-push/plugin/inspector/ios-inspector/ForgeModule/urbanairship_API.m \
-  /Users/pete/Dropbox/forge-workspace/trigger.io-ua-push/trigger.io-ua-push/plugin/inspector/ios-inspector/ForgeModule/urbanairship_API.h \
-  /Users/pete/Dropbox/forge-workspace/trigger.io-ua-push/trigger.io-ua-push/plugin/inspector/ios-inspector/ForgeModule/Airship/Library/PushLib/UAPush.h \
-  /Users/pete/Dropbox/forge-workspace/trigger.io-ua-push/trigger.io-ua-push/plugin/inspector/ios-inspector/ForgeModule/Airship/Common/UAGlobal.h \
-  /Users/pete/Dropbox/forge-workspace/trigger.io-ua-push/trigger.io-ua-push/plugin/inspector/ios-inspector/ForgeModule/Airship/Common/UAObservable.h \
-  /Users/pete/Dropbox/forge-workspace/trigger.io-ua-push/trigger.io-ua-push/plugin/inspector/ios-inspector/ForgeModule/Airship/Library/AirshipLib/UAirship.h \
-  /Users/pete/Dropbox/forge-workspace/trigger.io-ua-push/trigger.io-ua-push/plugin/inspector/ios-inspector/ForgeModule/Airship/Common/UAAnalytics.h \
+  /Users/zackd/dev/code/forge/trigger.io-ua-push/plugin/inspector/ios-inspector/ForgeModule/urbanairship_API.m \
+  /Users/zackd/dev/code/forge/trigger.io-ua-push/plugin/inspector/ios-inspector/ForgeModule/urbanairship_API.h \
+  /Users/zackd/dev/code/forge/trigger.io-ua-push/plugin/inspector/ios-inspector/ForgeModule/Airship/Library/PushLib/UAPush.h \
+  /Users/zackd/dev/code/forge/trigger.io-ua-push/plugin/inspector/ios-inspector/ForgeModule/Airship/Common/UAGlobal.h \
+  /Users/zackd/dev/code/forge/trigger.io-ua-push/plugin/inspector/ios-inspector/ForgeModule/Airship/Common/UAObservable.h \
+  /Users/zackd/dev/code/forge/trigger.io-ua-push/plugin/inspector/ios-inspector/ForgeModule/Airship/Library/AirshipLib/UAirship.h \
+  /Users/zackd/dev/code/forge/trigger.io-ua-push/plugin/inspector/ios-inspector/ForgeModule/Airship/Common/UAAnalytics.h \
   Airship/Common/UAHTTPConnection.h \
-  /Users/pete/Dropbox/forge-workspace/trigger.io-ua-push/trigger.io-ua-push/plugin/inspector/ios-inspector/ForgeModule/UAAppDelegateSurrogate.h \
-  /Users/pete/Dropbox/forge-workspace/trigger.io-ua-push/trigger.io-ua-push/plugin/inspector/ios-inspector/ForgeModule/Airship/Common/UALocationService.h \
+  /Users/zackd/dev/code/forge/trigger.io-ua-push/plugin/inspector/ios-inspector/ForgeModule/UAAppDelegateSurrogate.h \
+  /Users/zackd/dev/code/forge/trigger.io-ua-push/plugin/inspector/ios-inspector/ForgeModule/Airship/Common/UALocationService.h \
   Airship/Common/UALocationProviderDelegate.h \
   Airship/Common/UALocationProviderProtocol.h \
   Airship/Common/UALocationCommonValues.h \
