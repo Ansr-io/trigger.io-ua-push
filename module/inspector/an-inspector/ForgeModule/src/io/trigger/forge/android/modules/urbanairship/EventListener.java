6dc04515cc8456814f6502a10d91dd2bee2c30df
zack <zackdykes@gmail.com>
2013-08-19 12:04:15 +0100

    FAT-40: prevent crash on resume

--- module/inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/urbanairship/EventListener.java
+++ module/inspector/an-inspector/ForgeModule/src/io/trigger/forge/android/modules/urbanairship/EventListener.java
@@ -6,18 +6,44 @@ import io.trigger.forge.android.core.ForgeEventListener;
 import com.urbanairship.UAirship;
 import com.urbanairship.location.UALocationManager;
 import com.urbanairship.push.PushManager;
+import android.util.Log;
+
 
 public class EventListener extends ForgeEventListener {
+	
+	private static final String TAG = ">>>>> UA_MODULE";
+	
 	@Override
 	public void onApplicationCreate() {
+		
+		Log.v(TAG, "onApplicationCreate");
+		
 		UAirship.takeOff(ForgeApp.getApp());
 		PushManager.enablePush();
 		UALocationManager.init();
 		PushManager.shared().setIntentReceiver(IntentReceiver.class);
 	}
 	
+//	@Override
+//    public void onStop() {      
+//		Log.v(TAG, "onStop");
+//        UAirship.land(); 
+//    }
+	
+	
+	
+//	where these belong... ?
+	
 	@Override
+    public void onStart() {
+		Log.v(TAG, "onStop");
+        UAirship.shared().getAnalytics().activityStarted(ForgeApp.getActivity());
+    }
+
+    @Override
     public void onStop() {
-        UAirship.land();
+    	Log.v(TAG, "onStop");
+//    	UAirship.land();
+        UAirship.shared().getAnalytics().activityStopped(ForgeApp.getActivity());
     }
 }
