d29070040392497680421a8b962a425c2ca72999
zack <zackdykes@gmail.com>
2013-11-18 18:01:06 +0000

    FAT-66: review build_steps.json, update forgeInspector, add missing service declaration

---
+++ module/inspector/an-inspector/ForgeModule/gen/io/trigger/forge/android/modules/urbanairship/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package io.trigger.forge.android.modules.urbanairship;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
